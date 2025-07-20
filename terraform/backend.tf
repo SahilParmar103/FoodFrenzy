d59ea77a2575f32f7ccb5163a3256a8adcc81a4c
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-07-20 15:48:45 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3"
 
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
+    bucket         = "bucketforgitact"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
