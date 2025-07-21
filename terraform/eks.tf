201e51ed2b552cb7657bbc90dfc33de363aefadc
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-07-20 21:38:24 -0400

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,76 +1,39 @@
-resource "aws_iam_role" "eks_role" {
-  name = "eksClusterRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = { Service = "eks.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_policy_attach" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role" "node_role" {
-  name = "eksNodeGroupRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_eks_policy" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "node_cni_policy" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "node_registry_policy" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "19.15.1"
 
-  cluster_name    = "foodfrenzy-eks"
-  cluster_version = "1.28"
-
-  subnet_ids                     = module.vpc.private_subnets
-  vpc_id                         = module.vpc.vpc_id
+  cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
-  enable_irsa = true  # Enable IRSA for ALB ingress controller
-
-  iam_role_name   = aws_iam_role.eks_role.name
-  create_iam_role = false
-  iam_role_arn    = aws_iam_role.eks_role.arn  # ✅ Corrected here
-
-  eks_managed_node_groups = {
-    default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_types = ["t3.medium"]
-      iam_role_arn   = aws_iam_role.node_role.arn
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
     }
   }
 
-  tags = {
-    Environment = "dev"
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    panda-node = {
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+
+      instance_types = ["t2.medium"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        ExtraTag = "Panda_Node"
+      }
+    }
   }
+
+  tags = local.tags
 }
