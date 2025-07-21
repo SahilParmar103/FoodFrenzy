8e78f77758b663bdfecf02fb1861452d6d8fd1c6
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-07-20 21:42:00 -0400

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,199 +1,173 @@
-# -----------------------------
-# EC2 IAM Role for EKS Access
-# -----------------------------
-resource "aws_iam_role" "ec2_eks_access_role" {
-  name = "EC2EKSAccessRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-resource "aws_iam_role_policy" "eks_control_permissions" {
-  name = "EKSControlAccess"
-  role = aws_iam_role.ec2_eks_access_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "eks:DescribeCluster",
-          "eks:UpdateClusterConfig",
-          "eks:AccessKubernetesApi"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.67.0"
+    }
+  }
 }
 
-
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "EC2EKSAccessProfile"
-  role = aws_iam_role.ec2_eks_access_role.name
-}
-resource "aws_iam_role_policy_attachment" "eks_access_attach" {
-  role       = aws_iam_role.ec2_eks_access_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+provider "aws" {
+  region = var.region_name
 }
 
-
-# -----------------------------
-# Security Group
-# -----------------------------
+# STEP1: CREATE SG
 resource "aws_security_group" "my-sg" {
-  name        = "SonarserverPorts"
-  description = "Sonar Server Ports"
-  vpc_id      = module.vpc.vpc_id
-
+  name        = "JENKINS-SERVER-SG"
+  description = "Jenkins Server Ports"
+  
+  # Port 22 is required for SSH Access
   ingress {
-    description = "SSH Port"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    description     = "SSH Port"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Port 80 is required for HTTP
   ingress {
-    description = "HTTP Port"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    description     = "HTTP Port"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Port 443 is required for HTTPS
   ingress {
-    description = "HTTPS Port"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    description     = "HTTPS Port"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Port 2379-2380 is required for etcd-cluster
   ingress {
-    description = "Etcd Cluster"
-    from_port   = 2379
-    to_port     = 2380
-    protocol    = "tcp"
+    description     = "etc-cluster Port"
+    from_port       = 2379
+    to_port         = 2380
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }  
 
+  # Port 3000 is required for Grafana
   ingress {
-    description = "Grafana Port"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
+    description     = "NPM Port"
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }  
 
+  # Port 6443 is required for KubeAPIServer
   ingress {
-    description = "Kube API Server"
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
+    description     = "Kube API Server"
+    from_port       = 6443
+    to_port         = 6443
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }  
 
+  # Port 8080 is required for Jenkins
   ingress {
-    description = "Jenkins Port"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    description     = "Jenkins Port"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }  
 
+  # Port 9000 is required for SonarQube
   ingress {
-    description = "SonarQube Port"
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
+    description     = "SonarQube Port"
+    from_port       = 9000
+    to_port         = 9000
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }  
 
+  # Port 9090 is required for Prometheus
   ingress {
-    description = "Prometheus Port"
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
+    description     = "Prometheus Port"
+    from_port       = 9090
+    to_port         = 9090
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }  
 
+  # Port 9100 is required for Prometheus metrics server
   ingress {
-    description = "Prometheus Metrics Port"
-    from_port   = 9100
-    to_port     = 9100
-    protocol    = "tcp"
+    description     = "Prometheus Metrics Port"
+    from_port       = 9100
+    to_port         = 9100
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  } 
 
+  # Port 10250-10260 is required for K8s
   ingress {
-    description = "Kubernetes Ports"
-    from_port   = 10250
-    to_port     = 10260
-    protocol    = "tcp"
+    description     = "K8s Ports"
+    from_port       = 10250
+    to_port         = 10260
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }  
 
+  # Port 30000-32767 is required for NodePort
   ingress {
-    description = "K8s NodePort Range"
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
+    description     = "K8s NodePort"
+    from_port       = 30000
+    to_port         = 32767
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }  
 
+  # Define outbound rules to allow all
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# -----------------------------
-# EC2 Instance
-# -----------------------------
+# STEP2: CREATE EC2 USING PEM & SG
 resource "aws_instance" "my-ec2" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  key_name                    = var.ec2_key_pair
-  subnet_id                   = module.vpc.public_subnets[0]
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.my-sg.id]
-  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
-
+  ami           = var.ami   
+  instance_type = var.instance_type
+  key_name      = var.key_name        
+  vpc_security_group_ids = [aws_security_group.my-sg.id]
+  
   root_block_device {
     volume_size = var.volume_size
   }
-
+  
   tags = {
     Name = var.server_name
   }
-
+  
+    # USING REMOTE-EXEC PROVISIONER TO INSTALL PACKAGES
   provisioner "remote-exec" {
+    # ESTABLISHING SSH CONNECTION WITH EC2
     connection {
       type        = "ssh"
-      private_key = var.ec2_private_key
+      private_key = var.ec2_private_key # replace with your key-name 
       user        = "ubuntu"
       host        = self.public_ip
     }
 
     inline = [
       # Install AWS CLI
+      # Ref: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
       "sudo apt install unzip -y",
       "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'",
       "unzip awscliv2.zip",
       "sudo ./aws/install",
 
       # Install Docker
+      # Ref: https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
       "sudo apt-get update -y",
       "sudo apt-get install -y ca-certificates curl",
       "sudo install -m 0755 -d /etc/apt/keyrings",
@@ -206,10 +180,11 @@ resource "aws_instance" "my-ec2" {
       "sudo chmod 777 /var/run/docker.sock",
       "docker --version",
 
-      # Install SonarQube
+      # Install SonarQube (as container)
       "docker run -d --name sonar -p 9000:9000 sonarqube:lts-community",
 
       # Install Trivy
+      # Ref: https://aquasecurity.github.io/trivy/v0.18.3/installation/
       "sudo apt-get install -y wget apt-transport-https gnupg",
       "wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null",
       "echo 'deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main' | sudo tee -a /etc/apt/sources.list.d/trivy.list",
@@ -217,44 +192,55 @@ resource "aws_instance" "my-ec2" {
       "sudo apt-get install trivy -y",
 
       # Install Kubectl
+      # Ref: https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html#kubectl-install-update
       "curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.4/2024-09-11/bin/linux/amd64/kubectl",
       "curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.4/2024-09-11/bin/linux/amd64/kubectl.sha256",
       "sha256sum -c kubectl.sha256",
+      "openssl sha1 -sha256 kubectl",
       "chmod +x ./kubectl",
-      "sudo mv ./kubectl /usr/local/bin/kubectl",
+      "mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH",
+      "echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc",
+      "sudo mv $HOME/bin/kubectl /usr/local/bin/kubectl",
+      "sudo chmod +x /usr/local/bin/kubectl",
       "kubectl version --client",
 
       # Install Helm
+      # Ref: https://helm.sh/docs/intro/install/
+      # Ref (for .tar.gz file): https://github.com/helm/helm/releases
       "wget https://get.helm.sh/helm-v3.16.1-linux-amd64.tar.gz",
       "tar -zxvf helm-v3.16.1-linux-amd64.tar.gz",
       "sudo mv linux-amd64/helm /usr/local/bin/helm",
       "helm version",
 
       # Install ArgoCD
+      # Ref: https://argo-cd.readthedocs.io/en/stable/cli_installation/
       "VERSION=$(curl -L -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/VERSION)",
       "curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/v$VERSION/argocd-linux-amd64",
       "sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd",
-      "rm argocd-linux-amd64",
+      "rm argocd-linux-amd64", 
 
       # Install Java 17
+      # Ref: https://www.rosehosting.com/blog/how-to-install-java-17-lts-on-ubuntu-20-04/
       "sudo apt update -y",
       "sudo apt install openjdk-17-jdk openjdk-17-jre -y",
-      "java -version"
+      "java -version",
+
+    
     ]
   }
-}
+}  
 
-# -----------------------------
-# Outputs
-# -----------------------------
+# STEP3: GET EC2 USER NAME AND PUBLIC IP 
 output "SERVER-SSH-ACCESS" {
   value = "ubuntu@${aws_instance.my-ec2.public_ip}"
 }
 
+# STEP4: GET EC2 PUBLIC IP 
 output "PUBLIC-IP" {
-  value = aws_instance.my-ec2.public_ip
+  value = "${aws_instance.my-ec2.public_ip}"
 }
 
+# STEP5: GET EC2 PRIVATE IP 
 output "PRIVATE-IP" {
-  value = aws_instance.my-ec2.private_ip
+  value = "${aws_instance.my-ec2.private_ip}"
 }
