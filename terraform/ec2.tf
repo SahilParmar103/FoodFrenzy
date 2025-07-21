38704d74893fcc0a7afceb633c9ed86672953e3a
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-07-20 22:14:08 -0400

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -127,7 +127,8 @@ resource "aws_security_group" "my-sg" {
 resource "aws_instance" "my-ec2" {
   ami           = var.ami   
   instance_type = var.instance_type
-  key_name      = var.key_name        
+  key_name      = var.key_name
+  subnet_id     = module.vpc.public_subnets[0] 
   vpc_security_group_ids = [aws_security_group.my-sg.id]
   
   root_block_device {
