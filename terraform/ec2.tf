fff5d19b1ffa542ad75aa25215087afc7a7ed05e
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-07-20 20:03:12 -0400

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,3 +1,33 @@
+# -----------------------------
+# EC2 IAM Role for EKS Access
+# -----------------------------
+resource "aws_iam_role" "ec2_eks_access_role" {
+  name = "EC2EKSAccessRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_access_attach" {
+  role       = aws_iam_role.ec2_eks_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "EC2EKSAccessProfile"
+  role = aws_iam_role.ec2_eks_access_role.name
+}
+
+# -----------------------------
+# Security Group
+# -----------------------------
 resource "aws_security_group" "my-sg" {
   name        = "SonarserverPorts"
   description = "Sonar Server Ports"
@@ -107,13 +137,17 @@ resource "aws_security_group" "my-sg" {
   }
 }
 
+# -----------------------------
+# EC2 Instance
+# -----------------------------
 resource "aws_instance" "my-ec2" {
   ami                         = var.ami
   instance_type               = var.instance_type
   key_name                    = var.ec2_key_pair
   subnet_id                   = module.vpc.public_subnets[0]
-  associate_public_ip_address = true # ✅ This fixes the host resolution for remote-exec
+  associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.my-sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
 
   root_block_device {
     volume_size = var.volume_size
@@ -133,14 +167,12 @@ resource "aws_instance" "my-ec2" {
 
     inline = [
       # Install AWS CLI
-      # Ref: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
       "sudo apt install unzip -y",
       "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'",
       "unzip awscliv2.zip",
       "sudo ./aws/install",
 
       # Install Docker
-      # Ref: https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
       "sudo apt-get update -y",
       "sudo apt-get install -y ca-certificates curl",
       "sudo install -m 0755 -d /etc/apt/keyrings",
@@ -153,11 +185,10 @@ resource "aws_instance" "my-ec2" {
       "sudo chmod 777 /var/run/docker.sock",
       "docker --version",
 
-      # Install SonarQube (as container)
+      # Install SonarQube
       "docker run -d --name sonar -p 9000:9000 sonarqube:lts-community",
 
       # Install Trivy
-      # Ref: https://aquasecurity.github.io/trivy/v0.18.3/installation/
       "sudo apt-get install -y wget apt-transport-https gnupg",
       "wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null",
       "echo 'deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main' | sudo tee -a /etc/apt/sources.list.d/trivy.list",
@@ -165,59 +196,44 @@ resource "aws_instance" "my-ec2" {
       "sudo apt-get install trivy -y",
 
       # Install Kubectl
-      # Ref: https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html#kubectl-install-update
       "curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.4/2024-09-11/bin/linux/amd64/kubectl",
       "curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.4/2024-09-11/bin/linux/amd64/kubectl.sha256",
       "sha256sum -c kubectl.sha256",
-      "openssl sha1 -sha256 kubectl",
       "chmod +x ./kubectl",
-      "mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH",
-      "echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc",
-      "sudo mv $HOME/bin/kubectl /usr/local/bin/kubectl",
-      "sudo chmod +x /usr/local/bin/kubectl",
+      "sudo mv ./kubectl /usr/local/bin/kubectl",
       "kubectl version --client",
 
       # Install Helm
-      # Ref: https://helm.sh/docs/intro/install/
-      # Ref (for .tar.gz file): https://github.com/helm/helm/releases
       "wget https://get.helm.sh/helm-v3.16.1-linux-amd64.tar.gz",
       "tar -zxvf helm-v3.16.1-linux-amd64.tar.gz",
       "sudo mv linux-amd64/helm /usr/local/bin/helm",
       "helm version",
 
       # Install ArgoCD
-      # Ref: https://argo-cd.readthedocs.io/en/stable/cli_installation/
       "VERSION=$(curl -L -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/VERSION)",
       "curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/v$VERSION/argocd-linux-amd64",
       "sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd",
       "rm argocd-linux-amd64",
 
       # Install Java 17
-      # Ref: https://www.rosehosting.com/blog/how-to-install-java-17-lts-on-ubuntu-20-04/
       "sudo apt update -y",
       "sudo apt install openjdk-17-jdk openjdk-17-jre -y",
-      "java -version",
-
-
-
-      # Output
-      "echo 'Access SonarQube Server here --> http://'$ip':9000'",
-      "echo 'SonarQube Username & Password: admin'",
+      "java -version"
     ]
   }
 }
 
-# STEP3: GET EC2 USER NAME AND PUBLIC IP 
+# -----------------------------
+# Outputs
+# -----------------------------
 output "SERVER-SSH-ACCESS" {
   value = "ubuntu@${aws_instance.my-ec2.public_ip}"
 }
 
-# STEP4: GET EC2 PUBLIC IP 
 output "PUBLIC-IP" {
   value = aws_instance.my-ec2.public_ip
 }
 
-# STEP5: GET EC2 PRIVATE IP 
 output "PRIVATE-IP" {
   value = aws_instance.my-ec2.private_ip
 }
