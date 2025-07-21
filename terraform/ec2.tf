e1c53f4ac39c786272a5966624cf6b1e0f1cf9a5
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-07-20 21:45:52 -0400

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.67.0"
-    }
-  }
-}
 
-provider "aws" {
-  region = var.region_name
-}
 
 # STEP1: CREATE SG
 resource "aws_security_group" "my-sg" {
