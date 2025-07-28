dc08dcb6550f51efd833f44a98644be1f34d222b
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-07-28 14:23:43 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,3 +31,7 @@ variable "vpc_id" {
   description = "The ID of the VPC to associate the security group with"
   type        = string
 }
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
+  type        = string
+}
