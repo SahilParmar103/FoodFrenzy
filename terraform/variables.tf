3575c57aa7d58ee759df65ab43ee6dd597064cb6
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-07-28 11:39:06 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,3 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources in"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "ec2_key_pair" {
-  description = "Name of the EC2 Key Pair"
-  type        = string
-}
-
-variable "rds_db_username" {
-  description = "RDS master username"
-  type        = string
-}
-
-variable "rds_db_password" {
-  description = "RDS master password"
-  type        = string
-  sensitive   = true
-}
-
-variable "rds_instance_class" {
-  description = "RDS instance type"
-  type        = string
-  default     = "db.t3.micro"
-}
-
-variable "rds_storage_size" {
-  description = "Allocated storage for RDS"
-  type        = number
-  default     = 20
-}
 variable "instance_type" {
   description = "Instance Type"
   type        = string
@@ -59,11 +26,4 @@ variable "region_name" {
 variable "server_name" {
   description = "EC2 Server Name"
   type        = string
-}
-
-
-variable "ec2_private_key" {
-  description = "The private SSH key for EC2 remote-exec"
-  type        = string
-  sensitive   = true
 }
